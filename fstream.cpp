865f9358bbc73841a2fd1c1782718653f4a3d385
mila0408 <159796730+mila0408@users.noreply.github.com>
2024-11-24 18:54:24 +0300

    Add files via upload

---
+++ fstream.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include<map>
+using namespace std;
+
+string rus = "russian.txt";
+string eng = "english.txt";
+string dic = "dictionary.txt";
+
+
+int main()
+{
+    SetConsoleCP(1252);
+    SetConsoleOutputCP(1252);
+    map<string, string> p;
+    string line;
+    string rusline;
+    fstream in(dic);
+    if (in.is_open())
+    {
+        while (getline(in, line, ';')) {
+            getline(in, rusline);
+            p.insert(pair<string, string>(line, rusline));
+        }
+    }
+    in.close();
+    fstream out(rus, ios::app);
+    fstream in(eng);
+    if (out.is_open()&& in.is_open())
+    {
+        while (getline(in,line,';'))
+        {
+            out << p[line] << endl;
+        }
+        out.close();
+        in.close();
+    }
+}
