0d36371a7b9187c527d6560a0c28bae0d732e13b
mila0408 <159796730+mila0408@users.noreply.github.com>
2025-02-26 21:12:59 +0300

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+void menuAdmin() {
+	int choice;
+	bool flag = true;
+	while (flag) {
+		cout << "for further work, select the necessary action:" << endl;
+		cout << "1: grant admin rights" << endl;
+		cout << "2: add a category" << endl;
+		cout << "3: delete student" << endl;
+		cout << "4: change student data" << endl;
+		cout << "5: view the test results in general by category" << endl;
+		cout << "6: add new test" << endl;
+		cout << "7: change your date" << endl;
+		cout << "8: edit existing tests" << endl;
+		cout << "9: set correct and incorrect answers" << endl;
+		cout << "0: finish the job" << endl;
+		switch (choice)
+		{
+		case 1: {
+
+		}
+		case 2: {
+
+	    }
+		case 3: {
+
+		}
+		case 4: {
+
+		}
+		case 5: {
+
+		}
+		case 6: {
+
+		}
+		case 7: {
+
+		}
+		case 8: {
+
+		}
+		case 9: {
+
+		}
+		default:{
+			flag = false;
+			break;
+		}
+		}
+
+
+}
+
+}
+int main()
+{
+   
+}
